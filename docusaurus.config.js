54de5cf37d37253548d1d2c31555f202a4d8e832
pyang <p2yang@163.com>
2023-05-19 17:23:18 +0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/p2yang/p2yang.github.io/blob/main/blog/',
+            'https://github.com/p2yang/p2yang.github.io/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/p2yang/p2yang.github.io/blob/main/blog/',
+            'https://github.com/p2yang/p2yang.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
