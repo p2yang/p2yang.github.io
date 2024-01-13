b2dbba02d2b0ed6c5c381a5f2313c5ff7086fd1e
pyang <p2yang@163.com>
2024-01-13 22:57:42 +0800

    简化

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary')}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <Link
-          className="button button--secondary button--lg"
-          to="/blog">
-          Web Blog
-        </Link>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <main className="home">
+      <Link
+        className="btn-blog"
+        to="/blog">
+        Web Blog
+      </Link>
+    </main>
   );
 }
