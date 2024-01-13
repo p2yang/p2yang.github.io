1da9edb7718339ad45016edf6d434c2090271f1b
pyang <p2yang@163.com>
2024-01-13 22:16:56 +0800

    remove unused files

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,18 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary')}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog">
-            Web Blog
-          </Link>
-        </div>
+        <Link
+          className="button button--secondary button--lg"
+          to="/blog">
+          Web Blog
+        </Link>
       </div>
     </header>
   );
